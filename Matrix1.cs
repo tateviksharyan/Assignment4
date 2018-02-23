b47518025c2ad80c342cd685f1e6c538e9754415
Tatevik Sharyan <shtatev4991@gmail.com>
2018-02-24 00:29:06 +0400

    Assignment4 is almost completed

---
+++ Matrix1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+
+namespace Assignment4
+{
+    /// <summary>
+    /// The Matrix class
+    /// </summary>
+    class Matrix
+    {
+        private int[,] matrix;
+        private int rows { get; }
+        private int columns { get; }
+
+        /// <summary>
+        /// Creates the matrix with given number of rows and columns
+        /// </summary>
+        /// <param name="rows"> Number of rows </param>
+        /// <param name="columns"> Number of columns </param>
+        public Matrix(int rows, int columns)
+        {
+            matrix = new int[rows, columns];
+            Random random = new Random();
+            for(int i = 0; i < rows; ++i)
+            {
+                for(int j = 0; j < columns; ++j)
+                {
+                    matrix[i, j] = random.Next(100);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adds matrices
+        /// </summary>
+        /// <param name="matrix"> Operand </param>
+        /// <returns> Sum if two martices</returns>
+        public void addition(Matrix matrix)
+        {
+            if (this.rows == matrix.rows && this.columns == matrix.columns)
+            {
+                for (int i = 0; i < rows; ++i)
+                {
+                    for (int j = 0; j < columns; ++j)
+                    {
+                        this.matrix[i, j] += matrix.matrix[i, j];
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your matrices must have an equal number of rows and columns");
+            }
+        }
+
+        /// <summary>
+        /// Multiplies martices
+        /// </summary>
+        /// <param name="matrix"> Operand </param>
+        /// <returns> Multiplication of two matrices </returns>
+        public void multiplication(Matrix matrix)
+        {
+            if (this.columns == matrix.rows)
+            {
+                for (int i = 0; i < rows; ++i)
+                {
+                    for (int j = 0; j < columns; ++j)
+                    {
+                        this.matrix[i, j] *= matrix.matrix[i, j];
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("For multiply your first matrix's columns number and second matrix rows number must be the same.");
+            }
+        }
+
+        /// <summary>
+        /// Scalar multiplies matrices
+        /// </summary>
+        /// <param name="num"> Operand </param>
+        /// <returns> Scalar multiplication of the number and the matrix </returns>
+        public void scalarMultiplication(int num)
+        {
+            for (int i = 0; i < rows; ++i)
+            {
+                for (int j = 0; j < columns; ++j)
+                {
+                    matrix[i, j] *= num;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Inverse the matrix
+        /// </summary>
+        public void inverse()
+        {
+
+        }
+
+        /// <summary>
+        /// Transpose the matrix
+        /// </summary>
+        public Matrix transpose()
+        {
+            Matrix transposeMatrix = new Matrix(this.columns, this.rows);
+            int temp = 0;
+
+            for (int i = 0; i < transposeMatrix.rows; ++i)
+            {
+                for (int j = 0; j < transposeMatrix.columns; ++j)
+                {
+
+                    transposeMatrix.[i, j] = matrix[j, i];
+                }
+            }
+
+            return transposeMatrix;
+        }
+
+        /// <summary>
+        /// Determine whether a matrix is orthogonal or not
+        /// </summary>
+        /// <returns></returns>
+        public bool isOrthogonal()
+        {
+            Matrix identityMatrix = new Matrix(rows, rows);
+            multiplication(this.transpose());
+            return this.equals(identityMatrix);
+        }
+
+        /// <summary>
+        /// Checks matrix equality
+        /// </summary>
+        /// <param name="matrix"> Operand </param>
+        /// <returns> </returns>
+        public bool equals(Matrix matrix)
+        {
+            if (this.rows != matrix.rows || this.columns != matrix.columns)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i < rows; ++i)
+                {
+                    for (int j = 0; j < columns; ++j)
+                    {
+                        if (this.matrix[i, j] != matrix.matrix[i, j])
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Finds smallest element of matrix
+        /// </summary>
+        /// <returns> Smallest element </returns>
+        public int smallestElement()
+        {
+            int smallest = matrix[0, 0];
+
+            for (int i = 0; i < rows; ++i)
+            {
+                for (int j = 0; j < columns; ++j)
+                {
+                    if (matrix[i, j] <= smallest)
+                    {
+                        smallest = matrix[i, j];
+                    }
+                }
+            }
+            return smallest;
+        }
+
+        /// <summary>
+        /// Finds the largest element of matrix
+        /// </summary>
+        /// <returns> The largest element </returns>
+        public int largestElement()
+        {
+            int largest = matrix[0, 0];
+
+            for (int i = 0; i < rows; ++i)
+            {
+                for (int j = 0; j < columns; ++j)
+                {
+                    if (matrix[i, j] >= largest)
+                    {
+                        largest = matrix[i, j];
+                    }
+                }
+            }
+            return largest;
+        }
+
+
+        public void printMatrix()
+        {
+            for (int i = 0; i < rows; ++i)
+            {
+                for (int j = 0; j < columns; ++j)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                    if (j == columns - 1)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of rows: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the number of columns: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            if (rows < 0 || columns < 0)
+            {
+                Console.WriteLine("Please enter the correct number of rows and columns");
+            }
+
+            Matrix matrix = new Matrix(rows, columns);
+            Matrix newMatrix = new Matrix(rows, columns);
+
+            matrix.addition(newMatrix);
+            matrix.printMatrix();
+            
+        }
+
+    }
+}
